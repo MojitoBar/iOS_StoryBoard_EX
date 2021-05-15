98c1472e0aa2417445aca5306f76e53da86f948a
judongseok <wnehdtjr5@naver.com>
2021-05-16 00:33:59 +0900

    add Playground

---
+++ PlayGround/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+//
+//let value = arc4random_uniform(100)
+//print("-->\(value)")
+
+let coordinates = (4, 6)
+
+let x = coordinates.0
+let y = coordinates.1
+
+let coordinatedsNamed = (x: 2, y: 3)
+
+let x2 = coordinatedsNamed.x
+let y2 = coordinatedsNamed.y
+
+let (x3, y3) = coordinatedsNamed
+
+x3
+y3
