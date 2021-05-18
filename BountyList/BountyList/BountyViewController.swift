c80495c613dff9b233c738249b5ea4573377c022
MojitoBar <wnehdtjr5@gmail.com>
2021-05-19 00:27:51 +0900

    Study DetailViewController

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -2,12 +2,16 @@
 //  BountyViewController.swift
 //  BountyList
 //
-//  Created by judongseok on 2021/05/16.
+//  Created by judongseok on 2021/05/18.
 //
 
 import UIKit
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [33000000, 50, 44000000, 300000000, 16000000, 80000000, 77000000, 12000000]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,17 +20,43 @@ class BountyViewController: UIViewController, UITableViewDataSource, UITableView
     
     // UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return bountyList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+            return UITableViewCell()
+        }
+        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+        cell.imgView.image = img
+        cell.nameLabel.text = nameList[indexPath.row]
+        cell.bountyLabel.text = "\(bountyList[indexPath.row])"
         return cell
+        
+//        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell{
+//            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+//            cell.imgView.image = img
+//            cell.nameLabel.text = nameList[indexPath.row]
+//            cell.bountyLabel.text = "\(bountyList[indexPath.row])"
+//            return cell
+//        } else {
+//            return UITableViewCell()
+//        }
     }
     
     // UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("--> \(indexPath.row)")
+        
+        // showDetail 세그웨이를 보여줘라.
+        performSegue(withIdentifier: "showDetail", sender: nil)
     }
 }
+
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountyLabel: UILabel!
+}
+
+
